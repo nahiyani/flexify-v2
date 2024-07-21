248ba6c43eda2e71616335bb4d0a58801e28a186
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-07-21 13:02:34 -0400

    browserrouter updates

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs'; 
 import Classes from './pages/Classes';
 import Instructors from './pages/Instructors';
 import Library from './pages/Library';
 import Reserve from './pages/Reserve';
-import FAQ from './pages/faq'
+import FAQ from './pages/faq';
 
 function App() {
   return (
     <div>
-      <BrowserRouter basename='/flexify-v2'> 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<AboutUs />} /> 
-          <Route path='/classes' element={<Classes />} />
-          <Route path='/instructors' element={<Instructors />} />
-          <Route path='/library' element={<Library />} />
-          <Route path='/reserve' element={<Reserve />} />
-          <Route path='/faq' element={<FAQ />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutUs />} /> 
+        <Route path='/classes' element={<Classes />} />
+        <Route path='/instructors' element={<Instructors />} />
+        <Route path='/library' element={<Library />} />
+        <Route path='/reserve' element={<Reserve />} />
+        <Route path='/faq' element={<FAQ />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default App
