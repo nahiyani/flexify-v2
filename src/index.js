0d6a139ea768cef63a9cfd7a4d4feabe3aedd8de
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-07-21 13:05:59 -0400

    fixed small error

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './hometranslation';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/flexify-v2">  // Set the basename
+    <BrowserRouter basename="/flexify-v2">
       <App />
     </BrowserRouter>
   </React.StrictMode>
